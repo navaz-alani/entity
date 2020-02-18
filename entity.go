5d034f6bcb7418dfad9b76cfa68be6b21316554f
Navaz Alani <navaz.alani@crowdlinker.com>
2020-02-18 01:42:17 -0500

    updated import path

--- entity.go
+++ entity.go
@@ -82,8 +82,7 @@ import (
 
 	"go.mongodb.org/mongo-driver/mongo/options"
 
-	"entity/spec"
-
+	"github.com/navaz-alani/entity/spec"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
